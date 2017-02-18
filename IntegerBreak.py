75e8dda08ca47a56d12a1e7a04565bc022623764
catalystfrank <frankwang04@gmail.com>
2017-02-18 13:19:05 +0800

    Simplify IntegerBreak.py

--- IntegerBreak.py
+++ IntegerBreak.py
@@ -1,3 +1,3 @@
-def  IntegerBreak(number):
-    F = lambda n: 3**((n-2)/3)*2 if n%3==2 else 3**((n-4)/3)*4 if n%3==1 else 3**(n/3)
-    return int(F(number))
+#coding=utf-8
+
+IntegerBreak = lambda n: 3**((n-2)/3)*2 if n%3==2 else 3**((n-4)/3)*4 if n%3==1 else 3**(n/3)
