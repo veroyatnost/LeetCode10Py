f0bc362c14506953c369551ce00472ee1cd1d34c
bellumpara <zhangxiaohz@163.com>
2017-02-19 22:28:46 +0800

    debug

--- [CLOSED]LEET0001_TWO_SUM.py
+++ [CLOSED]LEET0001_TWO_SUM.py
@@ -9,3 +9,11 @@ def twoSum(nums, target):
     and you may not use the same element twice.
     """
     result = []
+    n = len(nums)
+    for i in range(n):
+        for j in range(i+1,n):
+            if target == nums[j] + nums[i]:
+                result.append(i)
+                result.append(j)
+                return result
+    return None
