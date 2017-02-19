8d46a3edbd77dc0c0fe86c3ff40fa2896f031a30
bellumpara <zhangxiaohz@163.com>
2017-02-19 22:25:05 +0800

    add a file

---
+++ [CLOSED]LEET0001_TWO_SUM.py
@@ -0,0 +1,11 @@
+def twoSum(nums, target):
+    """
+    :type nums: List[int] 
+    :type target: int
+    :rtype: List[int]
+    Given an array of integers, return indices of the two numbers such that 
+    they add up to a specific target.
+    You may assume that each input would have exactly one solution, 
+    and you may not use the same element twice.
+    """
+    result = []
