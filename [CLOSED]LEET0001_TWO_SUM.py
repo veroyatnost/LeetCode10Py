8877f6af2eddc1a4445649551be21d8c8b628c0a
bellumpara <zhangxiaohz@163.com>
2017-02-20 16:23:04 +0800

    add a solution

--- [CLOSED]LEET0001_TWO_SUM.py
+++ [CLOSED]LEET0001_TWO_SUM.py
@@ -1,12 +1,18 @@
-def twoSum(nums, target):
+#coding=utf-8
+
+'''
+https://leetcode.com/problems/two-sum/?tab=Description
+Given an array of integers, return indices of the two numbers such that 
+they add up to a specific target.
+you may not use the same element twice.
+'''
+
+
+def twoSum_1(nums, target):
     """
     :type nums: List[int] 
     :type target: int
     :rtype: List[int]
-    Given an array of integers, return indices of the two numbers such that 
-    they add up to a specific target.
-    You may assume that each input would have exactly one solution, 
-    and you may not use the same element twice.
     """
     result = []
     n = len(nums)
@@ -17,3 +23,15 @@ def twoSum(nums, target):
                 result.append(j)
                 return result
     return None
+
+
+#solution 2
+#base on hash
+
+def twoSum_2(nums, target):
+    dic = {}
+    for index, num in enumerate(nums):
+        if num in dic:
+            return [dic[num], index]
+        else:
+            dic[target - num] = index
