33bb4a7e7d666145ca9982e551157952cc32de9e
panyan <jackysc@qq.com>
2017-02-25 22:43:00 +0800

    add_two_numbers

--- [OPEN---]LEET0002_ADD_TWO_NUMBERS.py
+++ [OPEN---]LEET0002_ADD_TWO_NUMBERS.py
@@ -9,3 +9,24 @@ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 
 '''
+
+
+def add_two_numbers(number1,number2):
+    list1 = number1.split("->")
+    list2 = number2.split("->")
+    newlist = []
+    carry = 0
+    for n,m in zip(list1,list2):
+        if int(n) + int(m) + carry < 10:
+            newlist.append(str(int(n) + int(m) + carry))
+            carry = 0
+        else:
+            newlist.append(str(int(n) + int(m) + carry - 10))
+            carry = 1
+    return "->".join(newlist)
+
+if __name__ == "__main__":
+    num1 = "2 -> 4 -> 3"
+    num2 = "5 -> 6 -> 4"
+    print(add_two_numbers(num1,num2))
+
