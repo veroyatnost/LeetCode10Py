ddce182d1a47ca6bf1d61067384350c17a68800d
Earwin Yu <Rascht@users.noreply.github.com>
2017-03-04 23:50:25 +0800

    Update 0214 pull a solve with one line

--- [CLOSED]LEET0007_REVERSE_INTEGER.py
+++ [CLOSED]LEET0007_REVERSE_INTEGER.py
@@ -11,3 +11,8 @@ def reverse(x):
     t = list(reversed(str(x*compare)))
     x = compare * int(''.join(i for i in t))
     return x if abs(x) < 2 ** 31 else 0
+
+def reverse2(x):
+	return int(str(x)[::-1] if x>0 else "-"+(str(abs(x))[::-1]))
+
+reverseLambda = lambda x: int(str(x)[::-1] if x>0 else "-"+(str(abs(x))[::-1]))
