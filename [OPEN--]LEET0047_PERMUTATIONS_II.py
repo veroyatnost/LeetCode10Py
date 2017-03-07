a2216d97ff5e8f2408b105599583a2bbca05c779
bellumpara <zhangxiaohz@163.com>
2017-03-07 22:19:23 +0800

    add four file leet0046~leet0049 open

---
+++ [OPEN--]LEET0047_PERMUTATIONS_II.py
@@ -0,0 +1,12 @@
+#coding = utf-8
+
+#Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+#For example
+#[1,1,2] have the following unique permutations:
+#[
+#[1,1,2],
+#[1,2,1],
+#[2,1,1],
+#]
+
---
+++ [OPEN--]LEET0048_ROTATE_IMAGE.py
@@ -0,0 +1,8 @@
+#coding = utf-8
+
+#You are given an n x n 2D matrix representing an image.
+
+#Rotate the image by 90 degrees (clockwise).
+
+#Could you do this in-place?
+
