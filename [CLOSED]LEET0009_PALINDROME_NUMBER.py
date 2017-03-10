d4bc4d0921c8352486c305400b8668976f391563
Earwin Yu <Rascht@users.noreply.github.com>
2017-03-10 20:24:32 +0800

    Update [CLOSED]LEET0009_PALINDROME_NUMBER.py

--- [CLOSED]LEET0009_PALINDROME_NUMBER.py
+++ [CLOSED]LEET0009_PALINDROME_NUMBER.py
@@ -1,4 +1,12 @@
 #coding=utf-8
+"""
+0007 and 0009 are similar while solving one of them could refer to the other one
+
+version 1 of IsPalNumber using the basic way to solve 0009. However, PALINDROME NUMBER
+could be taken as string or even list. Int is not necessary.
+
+"""
+
 #version 1
 def is_pal_number(x):
     if x < 0:
