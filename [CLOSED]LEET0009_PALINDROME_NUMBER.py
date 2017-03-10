8fe0b5d339938445404dc3f854d871b03c03c4bc
Earwin Yu <Rascht@users.noreply.github.com>
2017-03-10 17:48:31 +0800

    Update [CLOSED]LEET0009_PALINDROME_NUMBER.py

--- [CLOSED]LEET0009_PALINDROME_NUMBER.py
+++ [CLOSED]LEET0009_PALINDROME_NUMBER.py
@@ -1,4 +1,5 @@
 #coding=utf-8
+#version 1
 def is_pal_number(x):
     if x < 0:
         return False
@@ -8,3 +9,6 @@ def is_pal_number(x):
         rev = rev * 10 + temp % 10
         temp = temp // 10
     return rev == x
+
+#version 2 in one line
+IsPalNumber = lambda x:  str(x) == str(x)[::-1]
