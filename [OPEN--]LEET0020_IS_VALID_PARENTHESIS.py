c5cc6f0464651dc05ccd727dfcdf7813a07daeaf
catalystfrank <frankwang.04@gmail.com>
2017-03-12 14:49:45 +0800

    finish 0020 is valid parenthesis

--- [OPEN--]LEET0020_IS_VALID_PARENTHESIS.py
+++ [OPEN--]LEET0020_IS_VALID_PARENTHESIS.py
@@ -1,1 +1,13 @@
-#coding=utf-8
+# coding=utf-8
+
+
+def isvalidparenthesis(a):
+    dec = {")": "(", "]": "[", "}": "{","(":"(","[":"[","{":"{"}
+    if len(a) % 2 != 0:
+        return False
+    else:
+        for n, m in [a[n * 2:(n + 1) * 2] for n in range(0, len(a) / 2)]:
+            if n != dec[m]:
+                return False
+    return True
+
