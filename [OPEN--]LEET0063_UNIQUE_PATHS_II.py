72331ce313885a517ab25432a8ce6be8fc9694bf
liujun0603 <michael_gemini@hotmail.com>
2017-03-22 23:52:17 +0800

    Update [OPEN--]LEET0063_UNIQUE_PATHS_II.py

--- [OPEN--]LEET0063_UNIQUE_PATHS_II.py
+++ [OPEN--]LEET0063_UNIQUE_PATHS_II.py
@@ -21,13 +21,14 @@ The total number of unique paths is 2.
 
 Note: m and n will be at most 100.
 """
-def Path(Grid):
-  m,n=len(Grid),len(Grid[0])
+def uniquePathsWithObstacles(obstacleGrid):
+  m,n=len(obstacleGrid),len(obstacleGrid[0])
   dp=[[0 for i in range(n)] for i in range(m)]
-  for i in range(n):
-    dp[0][i]=1 if s[0][i]==0 else 0
-  for i in range(m):
-    dp[i][0]=1 if s[i][0]==0 else 0
+  for j in range(n):
+    #time inefficiency
+    dp[0][j]=1 if obstacleGrid[0][:j+1].count(1)==0 else 0
+  for i in range(1,m):
+    dp[i][0]=1 if list(zip(*obstacleGrid))[0][:i+1].count(1)==0 else 0
     for j in range(1,n):
-      dp[i][j]=dp[i-1][j]+dp[i][j-1] if s[i][j]==0 else 0
+      dp[i][j]=dp[i-1][j]+dp[i][j-1] if obstacleGrid[i][j]==0 else 0
   return dp[m-1][n-1]
