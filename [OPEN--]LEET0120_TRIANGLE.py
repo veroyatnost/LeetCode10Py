bc6bbac5e1fbfa228f1a3ac513e4e0a4b4618164
liujun0603 <michael_gemini@hotmail.com>
2017-03-22 17:14:16 +0800

    Update [OPEN--]LEET0120_TRIANGLE.py

--- [OPEN--]LEET0120_TRIANGLE.py
+++ [OPEN--]LEET0120_TRIANGLE.py
@@ -14,3 +14,12 @@ For example, given the following triangle
 ]
 The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
 """
+def MinSum(t):
+     LSum,CSum=t[0],[0]
+     for i in range(1,len(t)):
+          CSum[0]=LSum[0]+t[i][0]
+          for j in range(1,len(LSum)):
+               CSum[j]=t[i][j]+min(LSum[j],LSum[j-1])
+          CSum.append(t[i][-1]+LSum[-1])
+          LSum=CSum.copy()
+     return min(CSum)
