b474d93a96ded770e5fb87d5109a60030136ebd3
liujun0603 <michael_gemini@hotmail.com>
2017-03-22 17:17:00 +0800

    Update and rename [OPEN--]LEET0032_LONGEST_VALID_PARENTHESIS.py to [CLOSED--]LEET0032_LONGEST_VALID_PARENTHESIS.py

--- [CLOSED--]LEET0032_LONGEST_VALID_PARENTHESIS.py
+++ [CLOSED--]LEET0032_LONGEST_VALID_PARENTHESIS.py
@@ -13,4 +13,13 @@ Subscribe to see which companies asked this question.
 """
 
 # Hint: CumSum, two-cursor, DP O(MN)
-
+def Paren(s,i):
+  return s[i-1:i+1]=='()'
+def ParenNum(s):
+  m=len(s)
+  dp=[0 for i in range(m)]
+  dp[0]=0
+  dp[1]=1 if Paren(s,1) else 0
+  for i in range(2,m):
+    dp[i]=dp[i-2]+1 if Paren(s,i) else dp[i-1]
+  return dp[m-1]
