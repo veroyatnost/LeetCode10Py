4e5d847b9eddb94e94694ca39029dd716438f661
bellumpara <zhangxiaohz@163.com>
2017-04-12 21:57:31 +0800

    add five lc file 126~130

---
+++ [OPEN--]LEET0126_WORD_LADDER_II.py
@@ -0,0 +1,27 @@
+
+
+
+'''
+Given two words (beginWord and endWord), and a dictionary's word list, find all shortest transformation sequence(s) from beginWord to endWord, such that:
+
+1.Only one letter can be changed at a time
+2.Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
+
+For example:
+Given:
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log","cog"]
+Return
+  [
+    ["hit","hot","dot","dog","cog"],
+    ["hit","hot","lot","log","cog"]
+  ]
+
+  Note:
+Return an empty list if there is no such transformation sequence.
+All words have the same length.
+All words contain only lowercase alphabetic characters.
+You may assume no duplicates in the word list.
+You may assume beginWord and endWord are non-empty and are not the same.
+'''
