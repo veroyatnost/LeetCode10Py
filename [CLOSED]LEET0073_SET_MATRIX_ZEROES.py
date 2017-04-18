88e965daaee71f4e96b29732d79807e217ccb255
liujun0603 <michael_gemini@hotmail.com>
2017-04-18 22:11:42 +0800

    Update and rename [OPEN--]LEET0073_SET_MATRIX_ZEROES.py to [CLOSED]LEET0073_SET_MATRIX_ZEROES.py

--- [CLOSED]LEET0073_SET_MATRIX_ZEROES.py
+++ [CLOSED]LEET0073_SET_MATRIX_ZEROES.py
@@ -11,3 +11,10 @@ A straight forward solution using O(mn) space is probably a bad idea.
 A simple improvement uses O(m + n) space, but still not the best solution.
 Could you devise a constant space solution?
 """
+
+def setZeros(M):
+  r=[0 in M[i] for i in range(len(M))]
+  c=[0 in set(M[i][j] for i in range(len(M))) for j in range(len(M[0]))]
+  for i in range(len(M)):
+    for j in range(len(M[0])):
+      M[i][j]=0 if r[i] or c[j] else M[i][j]
