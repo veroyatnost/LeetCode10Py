b7de6c39f0e1e1fea4d49fde9506a3b206aaec02
liujun0603 <michael_gemini@hotmail.com>
2017-04-20 14:40:45 +0800

    Update [CLOSED]LEET0069_SQUARE_ROOT.py

--- [CLOSED]LEET0069_SQUARE_ROOT.py
+++ [CLOSED]LEET0069_SQUARE_ROOT.py
@@ -14,3 +14,17 @@ def sqrt(val):
   while square_root ** 2 > val:
     square_root = (square_root + val/square_root) / 2
   return square_root
+
+"""
+binary search method
+"""
+def mySqrt(num):
+  l,r=0,num
+  while l<=r:
+    m=l+(r-l)/2
+    n=m*m
+    if n<=num:
+      l=m+1
+    else:
+      r=m-1
+  return l-1
